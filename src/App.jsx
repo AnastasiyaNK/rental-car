0315f2f2d5a69daf7cf0142fbba6bd04374fbceb
AnastasiyaNK <tsoka.anastasiya@gmail.com>
2024-02-13 23:00:51 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Favorites, Home, Main } from "pages";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Main />
+      <Favorites />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Home } from "./Home/Home";
+export { default as Main } from "./Main/Main";
+export { default as Favorites } from "./Favorites/Favorites";
